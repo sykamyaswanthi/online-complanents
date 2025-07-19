dd5a1e0b259685dd0f76d79ddef490548b130559
sykamyaswanthi <228a1a0487@risekrishnasaiprakasam.edu.in>
2025-07-19 11:59:30 +0530

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+require('dotenv').config();
+
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch((err) => console.error("❌ MongoDB Connection Error:", err));
